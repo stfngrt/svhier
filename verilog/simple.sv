eea53fc2a8e9a7a4329ebac6e0a2e526f35762d7
Stefan Engert <stefan.engert@mailbox.org>
2026-03-31 20:34:34 +0200

    initial

---
+++ verilog/simple.sv
@@ -0,0 +1,2 @@
+module Simple;
+endmodule
---
+++ verilog/test.sv
@@ -0,0 +1,21 @@
+module A;
+endmodule
+
+module B;
+endmodule
+
+module C;
+  A I0 ();
+  B I1 ();
+endmodule
+
+module X;
+endmodule
+
+module Y;
+endmodule
+
+module D;
+  X I0 ();
+  Y I1 ();
+endmodule
